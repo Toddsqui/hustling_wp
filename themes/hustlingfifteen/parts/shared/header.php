aa84d0928662bc8c7ae885267978fe1145c4e198
Todd Squitieri <todd.squitieri@alumni.newschool.edu>
2015-12-02 21:18:57 +0900

    linked up the other links in the nav bar while also adding the philosophy and the newsletter pages.

--- themes/hustlingfifteen/parts/shared/header.php
+++ themes/hustlingfifteen/parts/shared/header.php
@@ -1,8 +1,8 @@
 <div class="nav">
     <ul>
         <li><em><a href="<?php echo get_home_url(); ?>">Home</a></em></li>
-        <li><em><a href="#">Philosophy</a></em></li>
-        <li><em><a href="#">Newsletter</a></em></li>
+        <li><em><a href="<?php echo get_home_url(); ?>/Philosophy">Philosophy</a></em></li>
+        <li><em><a href="<?php echo get_home_url(); ?>/Newsletter">Newsletter</a></em></li>
         <li><em><a href="<?php echo get_home_url(); ?>/contact">Contact</a></em></li>
 
   </ul>
